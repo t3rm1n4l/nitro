18dff5824aa18d3b80d903cc08d667fddc06effc
Sarath Lakshman <sarathlakshman@slynux.com>
2017-02-15 19:23:55 +0530

    rename test
    Change-Id: I5c32e1bf75d3a16f8edc9945b3a96e15d0e7eda8

--- plasma/mvcc_test.go
+++ plasma/mvcc_test.go
@@ -261,7 +261,7 @@ func SkipTestPlasmaMVCCPerf(t *testing.T) {
 
 }
 
-func TestPlasmaRecoveryPoint(t *testing.T) {
+func TestMVCCRecoveryPoint(t *testing.T) {
 	os.RemoveAll("teststore.data")
 	s := newTestIntPlasmaStore(testSnCfg)
 
